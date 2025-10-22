a6b0686646f24611d19583ab0179bcf279e2ce31
alperrte <alpertemiz1907@hotmail.com>
2025-10-22 22:02:07 +0300

    Giriş sayfası oluşturuldu

---
+++ uni2clup-frontend/src/App.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+import "./App.css";
+import LoginPage from "./pages/LoginPage"; // Giriþ sayfamýzý içeri alýyoruz
+
+function App() {
+    return (
+        <div className="App">
+            <LoginPage />
+        </div>
+    );
+}
+
+export default App;
