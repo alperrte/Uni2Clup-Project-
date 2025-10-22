8e54f785cf9043946581e4e22bd8f590f62c4031
alperrte <alpertemiz1907@hotmail.com>
2025-10-22 22:45:06 +0300

    Migrationlar eklendi.

--- uni2clup-frontend/src/App.js
+++ uni2clup-frontend/src/App.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import "./App.css";
-import LoginPage from "./pages/LoginPage"; // Giriþ sayfamýzý içeri alýyoruz
+import LoginPage from "./p
+0,+ ages / LoginPage"; // Giriþ sayfamýzý içeri alýyoruz
 
 function App() {
     return (
