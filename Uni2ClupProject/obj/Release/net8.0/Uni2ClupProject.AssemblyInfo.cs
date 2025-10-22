5093377ab77c6f75f9f0d33f98962ee8bcd4d856
Nisa Camcı <nscmc34@gmail.com>
2025-10-22 17:08:51 +0300

    test

--- Uni2ClupProject/obj/Release/net8.0/Uni2ClupProject.AssemblyInfo.cs
+++ Uni2ClupProject/obj/Release/net8.0/Uni2ClupProject.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Uni2ClupProject")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7c2e0a747d3b3f5f3dd98d40a2f81ed244b9b37")]
 [assembly: System.Reflection.AssemblyProductAttribute("Uni2ClupProject")]
